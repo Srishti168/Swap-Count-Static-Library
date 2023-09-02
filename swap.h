dd960a281f254afe068ff5957912051153cf7ccf
Srishti168 <101701334+Srishti168@users.noreply.github.com>
2023-09-03 03:11:06 +0530

    Create swap.h

---
+++ swap.h
@@ -0,0 +1,5 @@
+int bubbleSort(int *arr,int n);
+int selectionSort(int *arr,int n);
+int insertionSort(int *arr,int n);
+int mergeSort(int *arr,int l,int r);
+int quickSort(int *arr,int low,int high);
